4671ef6442efffdf07725b16c394d7fcf9573005
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-06-24 18:27:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function toCase(text) {
   // write your code here
+	let lowerCase = text.toLowerCase();
+	let upperCase = text.toUpperCase();
+
+	return lowerCase + "-" + upperCase;
 }
 
 // DO not change the code below
